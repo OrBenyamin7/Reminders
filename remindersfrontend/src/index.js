221a9dd77b8249d8c2071386d7f8c28bb902af9b
Or Benyamin <orb1998@gmail.com>
2023-06-03 10:48:45 +0300

    finishing mobile modification - 13

--- remindersfrontend/src/index.js
+++ remindersfrontend/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { TaskContextProvider } from './context/TaskContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container } from 'react-bootstrap';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <TaskContextProvider>
-      <App />
-    </TaskContextProvider>
-  </React.StrictMode>
+  <Container>
+     <React.StrictMode>
+        <TaskContextProvider>
+          <App />
+        </TaskContextProvider>
+      </React.StrictMode>
+  </Container>
 );
