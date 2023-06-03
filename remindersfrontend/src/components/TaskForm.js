1df5b19fa96f2c49ba858f9fb05143829fd959ee
Or Benyamin <orb1998@gmail.com>
2023-06-03 08:16:23 +0300

    Integration with google calendar

--- remindersfrontend/src/components/TaskForm.js
+++ remindersfrontend/src/components/TaskForm.js
@@ -10,7 +10,67 @@ const TaskForm = () => {
     const [error, setError] = useState(null)
     const [emptyFields, setEmptyFields] = useState([])
 
+    var gapi = window.gapi
+    var CLIENT_ID = '572225403155-ev8smqcjlgqu14f3r7j17rt4dbncest0.apps.googleusercontent.com'
+    var API_KEY = 'AIzaSyC-GO_0z2MFOujlzACn97hRuUTMK2gUHtY'
+    var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+    var SCOPES = "https://www.googleapis.com/auth/calendar.events"
+
+
     const handleSubmit = async (e) => {
+        gapi.load('client:auth2', () => {
+            console.log('loaded client')
+
+            gapi.client.init({
+                apiKey: API_KEY,
+                clientId: CLIENT_ID,
+                discoveryDocs: DISCOVERY_DOCS,
+                scope: SCOPES,
+            })
+
+            gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+
+            gapi.auth2.getAuthInstance().signIn()
+            .then(() => {
+
+                var event = {
+                    'summary': 'Google I/O 2015',
+                    'location': '800 Howard St., San Francisco, CA 94103',
+                    'description': 'A chance to hear more about Google\'s developer products.',
+                    'start': {
+                      'dateTime': '2015-05-28T09:00:00-07:00',
+                      'timeZone': 'America/Los_Angeles'
+                    },
+                    'end': {
+                      'dateTime': '2015-05-28T17:00:00-07:00',
+                      'timeZone': 'America/Los_Angeles'
+                    },
+                    'recurrence': [
+                      'RRULE:FREQ=DAILY;COUNT=2'
+                    ],
+                    'attendees': [
+                      {'email': 'lpage@example.com'},
+                      {'email': 'sbrin@example.com'}
+                    ],
+                    'reminders': {
+                      'useDefault': false,
+                      'overrides': [
+                        {'method': 'email', 'minutes': 24 * 60},
+                        {'method': 'popup', 'minutes': 10}
+                      ]
+                    }
+                  }
+
+                  var request = gapi.client.calendar.events.insert({
+                    'calendarId': 'primary',
+                    'resource': event,
+                  })
+
+                  request.execute(event => {
+                    window.open(event.htmlLink)
+                  })
+            })
+        })
         e.preventDefault()
 
         const task = {description, due_date}
