9662805425727ed00e7de6d56988f0f20cdf7b27
Or Benyamin <orb1998@gmail.com>
2023-06-04 16:24:33 +0300

    advanced in the edit window

--- remindersfrontend/src/components/TaskDetails.js
+++ remindersfrontend/src/components/TaskDetails.js
@@ -1,52 +1,37 @@
-import { useTasksContext } from "../hooks/useTasksContext"
-
-//date fns
+import { useState } from 'react'
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import TaskEdit from '../components/TaskEdit'
+
+const TaskDetails = ({ task }) => {
+    const [showModal, setShowModal] = useState(false)
 
 
-const TaskDetails = ({task}) => {
-    const {dispatch } = useTasksContext()
+    const handleEdit = () => {
+        setShowModal(true)
+    };
 
-    const handleDelete = async () => {
-        const response = await fetch('/api/tasks/' + task._id, {
-            method: 'DELETE'
-        })
-        const json = await response.json()
-
-        if (response.ok) {
-            dispatch({type: 'DELETE_TASK', payload: json})
-        }
-    }
-
-    const handleEdit = async () => {
-        /*
-        const response = await fetch('/api/tasks/' + task._id, {
-            method: 'PATCH'
-        })
-        const json = await response.json()
-
-        if (response.ok) {
-            dispatch({type: 'UPDATE_TASK', payload: json})
-        }
-                    <span className="material-symbols-outlined" onClick={handleDelete}>delete</span>
-
-        */
-       if(task.due_date == null){
-        console.log("null date")
-       }
-    }
+    const closeModal = () => {
+        setShowModal(false)
+    };
 
     return (
         <div className="task-details">
             <h4>{task.description}</h4>
             {task.due_date !== null && (
-                <p><strong>dueDate: </strong>{formatDistanceToNow(new Date(task.due_date), { addSuffix: true })}</p>
+                <p><strong>Due date: </strong>{formatDistanceToNow(new Date(task.due_date), { addSuffix: true })}</p>
             )}            
-            <p>{formatDistanceToNow(new Date(task.createdAt), {addSuffix: true})}</p>
+            <p><strong>Created: </strong>{formatDistanceToNow(new Date(task.createdAt), {addSuffix: true})}</p>
             <span className="material-symbols-outlined" onClick={handleEdit}>edit</span>
-            <span className="material-symbols-outlined" onClick={handleDelete}>delete</span>
+
+            {/* Modal for editing task details */}
+            {showModal && (
+                <TaskEdit
+                    task={task}
+                    onClose={closeModal}
+                />
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default TaskDetails
---
+++ remindersfrontend/src/components/TaskEdit.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { useTasksContext } from "../hooks/useTasksContext"
+
+
+const TaskEdit = ({ task, onClose }) => {
+  const [editedDescription, setEditedDescription] = useState(task.description)
+  const [editedDueDate, setEditedDueDate] = useState(task.due_date)
+  const { dispatch } = useTasksContext()
+
+
+  const handleChangeDescription = (e) => {
+    setEditedDescription(e.target.value)
+  };
+
+  const handleChangeDueDate = (e) => {
+    setEditedDueDate(e.target.value)
+  };
+
+  const handleDelete = async () => {
+    const response = await fetch('/api/tasks/' + task._id, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        dispatch({ type: 'DELETE_TASK', payload: task._id });
+    }
+};
+
+  return (
+    <div className="modal-overlay">
+      <div>
+        <h4 className="headLineEditTask">Details</h4>
+        <div className="description-input">
+          <strong>Description: </strong>
+          <input
+            type="text"
+            value={editedDescription}
+            onChange={handleChangeDescription}
+          />
+        </div>
+        <div className="due-date-input">
+          <strong>Due date: </strong>
+          <input
+            type="Date"
+            defaultValue={editedDueDate ? editedDueDate.slice(0, 10) : ''}
+            onChange={handleChangeDueDate}
+          />
+        </div>
+        
+        <div className="modal-buttons">
+          <button className="btn btn-secondary" onClick={onClose}>
+            Cancel
+          </button>
+          <span className="material-symbols-outlined" onClick={handleDelete}>delete</span>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskEdit;
