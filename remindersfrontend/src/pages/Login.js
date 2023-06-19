8dd1e0ece6b0eb5b806dc0c3b2dffabd62057fdf
Or Benyamin <orb1998@gmail.com>
2023-06-19 08:31:06 +0300

    adding dark wallpaper

--- remindersfrontend/src/pages/Login.js
+++ remindersfrontend/src/pages/Login.js
@@ -13,25 +13,27 @@ const Login = () => {
   }
 
   return (
-    <form className="login" onSubmit={handleSubmit}>
-      <h3>Log In</h3>
-      
-      <label>Email address:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
-      />
-      <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
-      />
+    <div class = "wrapper">
+      <form className="login" onSubmit={handleSubmit}>
+        <h3>Log In</h3>
+        
+        <label>Email address:</label>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email} 
+        />
+        <label>Password:</label>
+        <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
 
-      <button disabled={isLoading}>Log in</button>
-      {error && <div className="error">{error}</div>}
-    </form>
+        <button disabled={isLoading}>Log in</button>
+        {error && <div className="error">{error}</div>}
+      </form>
+    </div>
   )
 }
 
