b74118f15cc94792c65afdf82d05b692d68538aa
Or Benyamin <orb1998@gmail.com>
2023-06-19 09:10:27 +0300

    small changes in the css file and login

--- remindersfrontend/src/pages/Login.js
+++ remindersfrontend/src/pages/Login.js
@@ -17,20 +17,24 @@ const Login = () => {
       <form className="login" onSubmit={handleSubmit}>
         <h3>Log In</h3>
         
-        <label>Email address:</label>
-        <input 
-          type="email" 
-          onChange={(e) => setEmail(e.target.value)} 
-          value={email} 
-        />
+        <div class="input-box">
+          <label>Email address:</label>
+          <input 
+            type="email" 
+            onChange={(e) => setEmail(e.target.value)} 
+            value={email} 
+          />
+        </div>
+
+        <div class="input-box">
         <label>Password:</label>
         <input 
           type="password" 
           onChange={(e) => setPassword(e.target.value)} 
           value={password} 
         />
-
-        <button disabled={isLoading}>Log in</button>
+        </div>
+        <button class="logInButton" disabled={isLoading}>Log in</button>
         {error && <div className="error">{error}</div>}
       </form>
     </div>
